8ff29a45d074f854c847687f3799da149ec9fdf5
Asderiner <asderiner@gmail.com>
2023-04-04 14:28:07 -0700

    orbit camera update
    rotation/translation/zooming

--- ProtocolBigTree_Unity3D/Assets/Scripts/Input/OrbitCameraMove.cs
+++ ProtocolBigTree_Unity3D/Assets/Scripts/Input/OrbitCameraMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 // Move camera with mouse in orbit prediction window 
@@ -12,7 +13,7 @@ public class OrbitCameraMove : MonoBehaviour
 
     // what percent of screen will be used as active area for moving (border)
     [SerializeField]
-    private float activeScreenPercent;
+    private float activeScreenPercent = 10f;
 
     [SerializeField]
     private Camera mainCamera;
@@ -31,31 +32,54 @@ public class OrbitCameraMove : MonoBehaviour
         {
             mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         }
+
     }
 
     void MoveCameraXZ(Vector3 direction)
     {
-        mainCamera.transform.position += new Vector3(-direction.x, 0, -direction.y) * cameraMovingSpeed;
+        mainCamera.transform.position += mainCamera.transform.right * -direction.x * cameraMovingSpeed;
+
+        mainCamera.transform.position += Vector3.ProjectOnPlane(mainCamera.transform.forward,new Vector3(0,1,0)) * -direction.y * cameraMovingSpeed; /// fucking magic
     }
 
-    void MoveCameraY(Vector3 direction)
+    void MoveCameraZoom(float direction)
     {
-        mainCamera.transform.position += -direction * zoomSpeed;
+        mainCamera.transform.position += mainCamera.transform.forward * zoomSpeed * direction;
     }
 
-    void RotateCameraY(float direction, float speed = 1.0f)
+    void RotateCameraYaw(float direction, float speed = 1.0f)
     {
-        mainCamera.transform.Rotate(0, 0,direction * speed);
+        mainCamera.transform.Rotate(mainCamera.transform.up, -direction * speed,Space.World);
+        
+        mainCamera.transform.eulerAngles = new Vector3(mainCamera.transform.rotation.eulerAngles.x, mainCamera.transform.rotation.eulerAngles.y, 0);
     }
 
-    void RotateCameraX(float direction, float speed = 1.0f)
+    void RotateCameraPitch(float direction, float speed = 1.0f)
     {
-        mainCamera.transform.Rotate(direction * speed, 0, 0);
+        mainCamera.transform.Rotate(mainCamera.transform.right, direction * speed, Space.World);
+       
+        mainCamera.transform.eulerAngles = new Vector3(mainCamera.transform.rotation.eulerAngles.x, mainCamera.transform.rotation.eulerAngles.y, 0);
     }
 
+    //check if pointer in active area
+    bool pointerInActiveArea(Vector3 mousePosition)
+    {
+        Vector2 mouseScreenPositionNormalised = new Vector2(mousePosition.x / screenSize.x, mousePosition.y / screenSize.y); // position from 0 to 1 in x/ 0 to 1 in y
+
+        if (mouseScreenPositionNormalised.x <= activeScreenPercent / 100f ||
+                mouseScreenPositionNormalised.x >= 1 - activeScreenPercent / 100f ||
+                mouseScreenPositionNormalised.y <= activeScreenPercent / 100f ||
+                mouseScreenPositionNormalised.y >= 1 - activeScreenPercent / 100f)
+        {
+            return true;
+        }
+
+        return false;
+    }
 
     private void Update()
     {
+        
         screenSize.x = Screen.width;
         screenSize.y = Screen.height;
 
@@ -63,33 +87,27 @@ public class OrbitCameraMove : MonoBehaviour
 
         float zoomAxis = GameManager.Instance.GetInputManager().GetMouseScrollWheelInput();
 
-        Vector2 mouseScreenPositionNormalised = new Vector2(mouseCoords.x / screenSize.x, mouseCoords.y / screenSize.y); // position from 0 to 1 in x/ 0 to 1 in y
-
         Vector3 windowCenter = new Vector3(screenSize.x / 2, screenSize.y / 2, 0);
         Vector3 direction = windowCenter - mouseCoords;
         bool rotateCamera = GameManager.Instance.GetInputManager().GetRightMouseButtonDown();
 
         if (rotateCamera == false)
         {
-            if (mouseScreenPositionNormalised.x <= activeScreenPercent / 100f ||
-                mouseScreenPositionNormalised.x >= 1 - activeScreenPercent / 100f ||
-                mouseScreenPositionNormalised.y <= activeScreenPercent / 100f ||
-                mouseScreenPositionNormalised.y >= 1 - activeScreenPercent / 100f)
+            if(pointerInActiveArea(mouseCoords))
             {
                 MoveCameraXZ(direction.normalized);
             }
         }
         else 
         {
-            RotateCameraY(direction.normalized.x);
-            RotateCameraX(direction.normalized.y);
+            RotateCameraYaw(Mathf.Clamp(direction.x / (screenSize.x / 2), -1.0f, 1.0f), 0.5f);
+            RotateCameraPitch(Mathf.Clamp(direction.y / (screenSize.y / 2), -1.0f, 1.0f), 0.5f);
         }
 
         if(zoomAxis != 0.0f)
         {
-            MoveCameraY(new Vector3(0, zoomAxis, 0));
+            MoveCameraZoom(zoomAxis);
         }
-
 
     }
 }
